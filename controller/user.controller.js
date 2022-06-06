8dfc0bd01cbda659421e547594ceb300e2eb1d84
Shahzad <mech1090@gmail.com>
2022-06-05 20:43:13 -0600

    validation login sinup logics done

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,19 +1,36 @@
 const User = require('../model/index')
 const bcrypt = require('bcrypt')
 const config = require('config')
+const {validateSchema} = require('../validator/user.validator')
 
 
 
 const getLoginForm = (req,res)=>{
     res.render('login/layout')
 }
-const login = (req,res)=>{}
+const login = async(req,res)=>{
+    const {email,password} = req.body
+    const fields = {email,password}
+    const findUser = await User.findOne({email})
+    if(!findUser){
+        return res.render('singup/layout',{message:'user does not exist please sign up'})
+    }
+    const matchPassword = await bcrypt.compare(password,findUser.password)
+    if(!matchPassword){
+        return res.render('login/layout',{message:'Credentials Wrong'})
+    }
+    return res.render('user/layout')
+}
 const getSignupForm = (req,res)=>{
     res.render('signup/layout')
 }
 const signup =async (req,res)=>{
     const {email,password} = req.body
     const fields = {email,password}
+    const {error,value} = validateSchema(fields)
+    if(error){
+        return res.render('signup/layout',{message:error.details[0].message})
+    }
     const findUser = await User.findOne({email})
     if(findUser){
         return res.render('login/layout',{message:'User already exists please login'})
